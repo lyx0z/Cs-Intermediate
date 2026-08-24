4603e95d7625a5b44978ab3972e3effc3a0879c5
Kucher Mykyta Serhiyovich <mykyta.kucher@digitecgalaxus.ch>
2026-08-24 13:59:16 +0200

    Exerciese done

---
+++ Cs-Intermediate/03-StaticClassesAndMethods/Calculator.cs
@@ -0,0 +1,29 @@
+﻿namespace _03_StaticClassesAndMethods;
+
+//Schreibe eine Klasse, die alle Grundfunktionalität eines Taschenrechners anbieten soll. Die Klasse und ihre Methoden soll statisch sein und entsprechend aufgerufen werden. Nutze Exceptions wo dies sinnvoll ist.
+public class Calculator
+{
+    public static void Sum(int x, int y)
+    {
+        var z = x + y;
+        Console.WriteLine(z);
+    }
+
+    public static void Difference(int x, int y)
+    {
+        var z = x - y;
+        Console.WriteLine(z);
+    }
+
+    public static void Multiplication(int x, int y)
+    {
+        var z = x * y;
+        Console.WriteLine(z);
+    }
+
+    public static void Division(int x, int y)
+    {
+        var z = x / y;
+        Console.WriteLine(z);
+    }
+}
---
+++ Cs-Intermediate/03-StaticClassesAndMethods/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace _03_StaticClassesAndMethods;
+
+public static class StaticClassesAndMethods
+{
+    public static void Main(String[] args)
+    {
+        var x = 5;
+        var y = 10;
+        Calculator.Sum(x, y);
+    }
+}
