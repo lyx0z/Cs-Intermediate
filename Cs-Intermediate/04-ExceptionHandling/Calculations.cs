747aacbf736884875f6db9e77b771966eaab0113
Kucher Mykyta Serhiyovich <mykyta.kucher@digitecgalaxus.ch>
2026-08-24 17:25:04 +0200

    First Task done(?) looks wrong

---
+++ Cs-Intermediate/04-ExceptionHandling/Calculations.cs
@@ -0,0 +1,14 @@
+﻿namespace _04_ExceptionHandling;
+
+public class Calculations
+{
+    //Schreibe eine Methode, welche als Input eine Ganzzahl entgegennimmt und überprüft ob diese positiv ist. Falls nicht soll eine Exception geworfen werden.
+    public static void IsPositive(int userNumber)
+    {
+        if (userNumber < 0)
+        {
+            throw new ArithmeticException("Your number is negative");
+        }
+        Console.WriteLine("Your number is Positive");
+    }
+}
---
+++ Cs-Intermediate/04-ExceptionHandling/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace _04_ExceptionHandling;
+
+public static class ExceptionHandling
+{
+    public static void Main(String[] args)
+    {
+        Console.WriteLine("Input your Number");
+        int.TryParse(Console.ReadLine(), out var userNumber);
+        Calculations.IsPositive(userNumber);
+    }
+}
