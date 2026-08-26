6d6efdb90bdfd74e470f1110f8ef67c2aa69c32c
Kucher Mykyta Serhiyovich <mykyta.kucher@digitecgalaxus.ch>
2026-08-26 17:26:59 +0200

    Task 1 christmas tree

---
+++ Cs-Intermediate/06-AdvancedTasks/DrawingMaker.cs
@@ -0,0 +1,16 @@
+﻿namespace _06_AdvancedTasks;
+
+public class DrawingMaker
+{
+    public void Draw(bool[,] matrix)
+    {
+        for (int row = 0; row < matrix.GetLength(0); row++)
+        {
+            for (int col = 0; col < matrix.GetLength(1); col++)
+            {
+                Console.Write(matrix[row, col] ? 'x' : ' ');
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Cs-Intermediate/06-AdvancedTasks/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _06_AdvancedTasks;
+
+public class AdvancedTasks
+{
+    public static void Main(String[] args)
+    {
+        //Erweitere das Programm so, dass es das Array auf die Konsole schreiben kann, Werte welche false sind, sollen Leerzeichen sein, true sollen etwas schreiben, z.B. ein 'x'
+        var matrix = new bool[6, 9]
+        {
+            { false, false, false, false, true, false, false, false, false },
+            { false, false, false, true, true, true, false, false, false },
+            { false, false, true, true, true, true, true, false, false },
+            { false, true, true, true, true, true, true, true, false },
+            { true, true, true, true, true, true, true, true, true },
+            { false, false, false, false, true, false, false, false, false },
+        };
+        var draw = new DrawingMaker();
+        draw.Draw(matrix);
+    }
+}
