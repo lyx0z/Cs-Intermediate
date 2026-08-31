1aa83758f1fd865da1fb64325e3091534be19538
Kucher Mykyta Serhiyovich <mykyta.kucher@digitecgalaxus.ch>
2026-08-31 15:33:25 +0200

    Two Sum

---
+++ Cs-Intermediate/Extra-TwoSum/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Extra_TwoSum;
+
+public static class Program
+{
+    public static void Main(String[] args)
+    {
+        int[] numbers = [15, 7, 11, 2];
+        var target = 9;
+        var twoSummed = TwoSumMethod.TwoSum(numbers, target);
+
+        foreach (var num in twoSummed)
+        {
+            Console.WriteLine(num);
+        }
+    }
+}
---
+++ Cs-Intermediate/Extra-TwoSum/TwoSum.cs
@@ -0,0 +1,20 @@
+﻿namespace Extra_TwoSum;
+
+public class TwoSumMethod
+{
+    public static int[] TwoSum(int[] nums, int target)
+    {
+        for (var index = 0; index < nums.Length; index++)
+        {
+            for (var nextIndex = index + 1; nextIndex < nums.Length; nextIndex++)
+            {
+                if (nums[index] + nums[nextIndex] == target)
+                {
+                    return [index, nextIndex];
+                }
+            }
+        }
+
+        return [];
+    }
+}
