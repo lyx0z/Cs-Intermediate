0a8034ca3d8cf89e7c10c5fc85ea7b30222322ff
Kucher Mykyta Serhiyovich <mykyta.kucher@digitecgalaxus.ch>
2026-09-01 09:10:40 +0200

    Comments resolved

--- Cs-Intermediate/Extra-TwoSum/Program.cs
+++ Cs-Intermediate/Extra-TwoSum/Program.cs
@@ -2,15 +2,30 @@
 
 public static class Program
 {
-    public static void Main(String[] args)
+    public static void Main()
     {
         int[] numbers = [15, 7, 11, 2];
         var target = 9;
-        var twoSummed = TwoSumMethod.TwoSum(numbers, target);
+        var twoSummed = TwoSum(numbers, target);
 
         foreach (var num in twoSummed)
         {
             Console.WriteLine(num);
         }
+    }
+
+    private static int[] TwoSum(int[] nums, int target)
+    {
+        for (var index = 0; index < nums.Length; index++)
+        {
+            for (var nextIndex = index + 1; nextIndex < nums.Length; nextIndex++)
+            {
+                if (nums[index] + nums[nextIndex] == target)
+                {
+                    return [index, nextIndex];
+                }
+            }
+        }
+        return [];
     }
 }
